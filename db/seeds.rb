6d26b9894951cb729b3f3fb6c05b0cce52bc6dc3
shane <willyums86@gmail.com>
2021-10-06 09:05:27 -0400

    Adds New Places API

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+
+Place.create!([
+  { name: "Shane Williams", address: "123 Fake St" },
+  { name: "Johnnie Walker", address: "456 Drunk Cir" },
+  { name: "Jack Daniels", address: "654 Wasted Ave"},
+  { name: "Jim Beam", address: "999 Kentucky Blvd"}
+  ])
+
